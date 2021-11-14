85d27ce54fe1d0e448ade51c18106841f01eacb7
saivardhanreddie <93024740+saivardhanreddie@users.noreply.github.com>
2021-11-14 11:44:31 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,70 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        byte b = 3;
+        System.out.println("The byte value is : " + b);
+
+        short a = 178;
+        System.out.println("The short value is : " + a);
+
+        int x = 12345678;
+        System.out.println("The integer value is : " + x);
+
+        long l = 1234567890;
+        System.out.println("The long value is : " + l);
+
+        float f = 123.4f;
+        System.out.println("The float value is : " + f);
+
+        double d = 123.456789012;
+        System.out.println("The double value is : " + d);
+
+        char ch = 'v';
+        System.out.println("The character value is : " + ch);
+
+        String str = "sai vardhan reddy";
+        System.out.println("The String value is : " + str);
+
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println("The boolean value is : " + b1);
+        System.out.println("The boolean value is : " + b2);
+
+    }
+
+    }
+
+    
+      
+    
+
+
+    
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+    
+
